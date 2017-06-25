2c57d35b4f11ec12bd33a06131e7e36f80b13482
Stayko Chalakov <chalakov88@gmail.com>
2017-06-25 17:13:42 +0100

    Adding index.html and making some JS changes

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,6 @@
+/*
+	Card class
+*/
 function Card(rank, suit){
 	this.rank = rank;
   this.suit = suit;
@@ -7,41 +10,54 @@ Card.prototype.show = function(){
 	return this.rank + ' of ' + this.suit;
 }
 
-function Deck(){
+/*
+	Deck - Singleton class
+*/
+var Deck = new function(){
 	this.ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 	this.suits = ['HEARTS', 'SPADES', 'DIAMONDS','CLUBS'];
   this.deck = [];
+
+	this.init = function(){
+		for(var s = 0; s < this.suits.length; s++){
+	  	for(var r = 0; r < this.ranks.length; r++){
+	    	this.deck.push(new Card(this.ranks[r], this.suits[s]));
+	    }
+	  }
+	}
+
+	this.show = function(){
+		for(var d = 0; d < this.deck.length; d++){
+	  	console.log(this.deck[d].show());
+	  }
+	}
+
+	this.shuffle = function(){
+		 var j, x, i;
+	   for (i = this.deck.length; i; i--) {
+	       j = Math.floor(Math.random() * i);
+	       x = this.deck[i - 1];
+	       this.deck[i - 1] = this.deck[j];
+	       this.deck[j] = x;
+	   }
+	}
+
+	this.deal = function(){
+		var dealer = [], player = [];
+		dealer.push(this.deck.pop());
+		player.push(this.deck.pop());
+		player.push(this.deck.pop());
+
+		console.log(dealer);
+		console.log(player);
+	}
+
 }
 
-Deck.prototype.init = function(){
-	for(var s = 0; s < this.suits.length; s++){
-  	for(var r = 0; r < this.ranks.length; r++){
-    	this.deck.push(new Card(this.ranks[r], this.suits[s]));
-    }
-  }
+function Dealer(){
+	
 }
 
-Deck.prototype.show = function(){
-	for(var d = 0; d < this.deck.length; d++){
-  	console.log(this.deck[d].show());
-  }
-}
-
-Deck.prototype.shuffle = function(){
-	 var j, x, i;
-   for (i = this.deck.length; i; i--) {
-       j = Math.floor(Math.random() * i);
-       x = this.deck[i - 1];
-       this.deck[i - 1] = this.deck[j];
-       this.deck[j] = x;
-   }
-}
-
-Deck.prototype.deal = function(){
-
-}
-
-var d = new Deck();
-d.init();
-d.shuffle();
-d.show();
+Deck.init();
+Deck.shuffle();
+Deck.deal();
